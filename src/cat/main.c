8050ad12698a7bc23c6318b473585db65dc90521
AlexanderShilov <scrib.ru@mail.ru>
2022-08-25 22:02:46 +0700

    rename cat

--- src/cat/main.c
+++ src/cat/main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include "s21_cat.h"
+#include "my_cat.h"
 
 
 int main(int argc, char *argv[]) {
-    s21_cat(argc, argv);
+    my_cat(argc, argv);
     return 0;
 }
